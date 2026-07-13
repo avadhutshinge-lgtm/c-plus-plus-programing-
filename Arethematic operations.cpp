55bcaff9a543fea89e4e9817600d088de75f261a
avadhutshinge-lgtm <avadhutshinge1976@gmail.com>
2026-07-13 10:56:04 +0530

    Add files via upload

---
+++ Arethematic operations.cpp
@@ -0,0 +1,18 @@
+//Arethematic operations
+#include<iostream>
+using namespace std;
+int main()
+{
+float a=4,b=5,sum,sub,multi,div,mod;
+sum=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+mod=a%b;
+cout<<"*******your operations******"<<end1;
+cout<<"addition="<<sum;
+cout<<"Substraction="<<sub;
+cout<<"Multiplication="<<multi;
+cout<<"division="<<div;
+cout<<"modulus="<<mod;
+}
---
+++ addition.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a=3,b=5,c;
+c=a+b;
+cout<<"addition="<<c;
+}
+
+
