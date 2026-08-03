8219151c4fb84321a4ed2fdb121b9aa2ea043ad0
Avadhut Shinge <avadhutshinge1976@gmail.com>
2026-08-03 04:25:38 +0000

    area

---
+++ area_of_rec.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int l,b;
+float area;
+cout<<"enter the length="<<endl;
+cin>>l;
+cout<<"enter the breadth=";
+cin>>b;
+area=l*b;
+cout<<"Area of rectangle="<<area;
+}
